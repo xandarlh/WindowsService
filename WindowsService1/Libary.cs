dc7cf3144954da945d9e4939b58ca91e0576626a
Alexandar Lackovic Hansen <alex303a@zbc.dk>
2020-11-13 12:53:29 +0100

    d

---
+++ WindowsService1/Libary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsService1
+{
+    public static class Libary
+    {
+        public static void WriteErrorLog(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex.Source.ToString().Trim() + "; " + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+        }
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message );
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ WindowsService1/Scheduler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService1
+{
+    public partial class Scheduler : ServiceBase
+    {
+        private Timer timer1 = null;
+
+        public Scheduler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1 = new Timer();
+            this.timer1.Interval = 30000; //every 30 seconds
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Tick);
+            timer1.Enabled = true;
+            Libary.WriteErrorLog("Test windows service started");
+        }
+        private void timer1_Tick(object sender, ElapsedEventArgs e)
+        {
+            Libary.WriteErrorLog("Timer ticked and some job has been done succesfully");
+        }
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+            Libary.WriteErrorLog("Test windows service stopped");
+        }
+    }
+}
